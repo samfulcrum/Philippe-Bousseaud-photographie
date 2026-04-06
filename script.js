f9b5af33cdbb780a99377e25b914ae76ea3a3aa2
samfulcrum <sam.bousseaud@gmail.com>
2026-04-06 16:45:01 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const images = document.querySelectorAll('.gallery-item');
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.getElementById('lightbox-img');
+const closeBtn = document.querySelector('.close-btn');
+
+images.forEach(img => {
+    img.addEventListener('click', () => {
+        lightbox.style.display = 'flex';
+        lightboxImg.src = img.src;
+    });
+});
+
+closeBtn.addEventListener('click', () => {
+    lightbox.style.display = 'none';
+});
+
+lightbox.addEventListener('click', (e) => {
+    if(e.target !== lightboxImg) {
+        lightbox.style.display = 'none';
+    }
+});
